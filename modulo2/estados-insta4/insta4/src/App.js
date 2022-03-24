d6c08e00ce8c8880ef055e84963973d55b720f26
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-03-24 15:24:44 -0300

    fazendo implementações

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50?random=1'}
+          fotoPost={'https://picsum.photos/200/150?random=2'}
+        />
+        <Post
+          nomeUsuario={'rodrigo'}
+          fotoUsuario={'https://picsum.photos/50/50?random=3'}
+          fotoPost={'https://picsum.photos/200/150?random=4'}
+        />
+        <Post
+          nomeUsuario={'mina'}
+          fotoUsuario={'https://picsum.photos/50/50?random=5'}
+          fotoPost={'https://picsum.photos/200/150?random=6'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
