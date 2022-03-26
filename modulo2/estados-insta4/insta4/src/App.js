13964cb6924d3bc8925e338c7e9c097f455e5451
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-03-25 22:39:04 -0300

    fazendo implementação de lista

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,28 +10,86 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+  state = {
+    posts: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50?random=1',
+        fotoPost: 'https://picsum.photos/200/150?random=2'
+      },
+
+      {
+        nomeUsuario: 'rodrigo',
+        fotoUsuario: 'https://picsum.photos/50/50?random=3',
+        fotoPost: 'https://picsum.photos/200/150?random=4'
+      },
+
+      {
+        nomeUsuario: 'mina',
+        fotoUsuario: 'https://picsum.photos/50/50?random=5',
+        fotoPost: 'https://picsum.photos/200/150?random=6'
+      }
+    ],
+
+    valorInputNome: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+  }
+
+  adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+    }
+
+    const novoPosts = [...this.state.posts, novoPost]
+
+    this.setState({
+      posts: novoPosts,
+      valorInputNome: "",
+      valorInputFotoUsuario: "",
+      valorInputFotoPost: ""
+    })
+  }
+
+  //parei aqui
+
   render() {
+    const listaDePosts = this.state.posts.map((post) => {
+      return (
+        <Post
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
+      )
+    })
+
     return (
       <MainContainer>
-
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?random=1'}
-          fotoPost={'https://picsum.photos/200/150?random=2'}
-        />
-
-        <Post
-          nomeUsuario={'rodrigo'}
-          fotoUsuario={'https://picsum.photos/50/50?random=3'}
-          fotoPost={'https://picsum.photos/200/150?random=4'}
-        />
-
-        <Post
-          nomeUsuario={'mina'}
-          fotoUsuario={'https://picsum.photos/50/50?random=5'}
-          fotoPost={'https://picsum.photos/200/150?random=6'}
-        />
-        
+        <div>
+          {listaDePosts}
+        </div>
+        <div>
+          <input
+          value={this.state.valorInputNome}
+          onChange={}
+          placeholder={"Nome"}
+          />
+          <input
+          value={this.state.valorInputFotoUsuario}
+          onChange={}
+          placeholder={"Foto do usuário"}
+          />
+          <input
+          value={this.state.valorInputFotoPost}
+          onChange={}
+          placeholder={"Foto do post"}
+          />
+          <button>Enviar</button>
+        </div>
       </MainContainer>
     );
   }
