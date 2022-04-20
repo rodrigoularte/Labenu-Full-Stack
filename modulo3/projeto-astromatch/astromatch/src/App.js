558ccd3bddc14d9e2465654f410dcb508ee6f31b
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-20 15:26:06 -0300

    Projeto Astromatch

---
+++ modulo3/projeto-astromatch/astromatch/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      Astromatch
+    </div>
+  )
+}
+
+export default App
---
+++ modulo3/projeto-astromatch/astromatch/src/constants/variaveis.js
@@ -0,0 +1,1 @@
+export const urlBase = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/rodrigo-goularte-shaw/"
