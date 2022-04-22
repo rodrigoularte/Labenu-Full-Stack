3f531d618db5e50df5fa5a2fc0326d31647f4b7e
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-22 12:21:37 -0300

    mostrar perfil e funcionalidade da troca de perfil ok

--- modulo3/projeto-astromatch/astromatch/src/pages/Home.jsx
+++ modulo3/projeto-astromatch/astromatch/src/pages/Home.jsx
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import { urlBase } from "../constants/variaveis"
 
 function Home() {
 
+  const [perfil, setPerfil] = useState({})
+
+  const mostraPerfil = () => {
+    axios
+    .get(`${urlBase}/person`)
+    .then((res) => {
+      setPerfil(res.data.profile)
+    })
+    .catch(err => {console.log(err)})
+  }
+
+  useEffect(() => {mostraPerfil()}, [])
+
+  console.log(perfil)
+
   return(
     <div>
-      HOME
-      <button>Descartar</button>
+      <img src={perfil.photo} alt={perfil.name} />
+      <h2>{perfil.name}, {perfil.age}</h2>
+      <p>{perfil.bio}</p>
+      
+      <button onClick={mostraPerfil}>Descartar</button>
       <button>Like</button>
     </div>
   )
