063b64f4e59a6d32442b63dcf6b8da457099a4c8
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-26 17:38:02 -0300

    fazendo layout HomePage

--- modulo3/projeto-labex/labex/src/pages/HomePage.js
+++ modulo3/projeto-labex/labex/src/pages/HomePage.js
@@ -3,7 +3,11 @@ import React from "react"
 function HomePage() {
   return (
     <div>
-      HomePage
+      <div> <img></img> </div>
+      <div>
+        <h2>Encontre as melhores viagens espaciais!</h2>
+        <button>Encontre o destino ideal</button>
+      </div>
     </div>
   )
 }
