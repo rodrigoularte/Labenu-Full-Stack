bca1704bac8bde47d1649bf4f1a42a92a53fb280
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-26 12:03:46 -0300

    pasta pages adicionada

---
+++ modulo3/projeto-labex/labex/src/pages/ApplicationFormPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ApplicationFormPage() {
+  return (
+    <div>
+      ApplicationFormPage
+    </div>
+  )
+}
+
+export default ApplicationFormPage
--- modulo3/projeto-labex/labex/src/pages/HomePage.js
+++ modulo3/projeto-labex/labex/src/pages/HomePage.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-function App() {
+function HomePage() {
   return (
     <div>
-      LabeX
+      HomePage
     </div>
   )
 }
 
-export default App
+export default HomePage
