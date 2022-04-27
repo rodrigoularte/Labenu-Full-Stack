1ddd1acef6e0e110b64a356d8e9d71a718145b6b
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-26 22:59:23 -0300

    API funcionando na ListTripsPage e TripCard criado

--- modulo3/projeto-labex/labex/src/pages/ListTripsPage.js
+++ modulo3/projeto-labex/labex/src/pages/ListTripsPage.js
@@ -1,17 +1,50 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 import { useNavigate } from "react-router"
 import Header from "../components/Header"
 import {goToApplicationFormPage} from "../routes/coordinator"
+import { urlBase } from "../constants/constants"
+import TripCard from "../components/TripCard"
+
 
 function ListTripsPage() {
 
   const navigate = useNavigate()
+
+  const [trips, setTrips] = useState([])
+
+  const getTrips = () => {
+    axios
+    .get(`${urlBase}/trips`)
+    .then((res) => {
+      setTrips(res.data.trips)
+    })
+    .catch((err) => {console.log(err)})
+  }
+
+  useEffect(() => {getTrips()}, [])
+
+  const tripList = trips.map((trip) => {
+
+    return(
+      <TripCard key={trip.id}
+        name={trip.name}
+        description={trip.description}
+        planet={trip.planet}
+        durationInDays={trip.durationInDays}
+        date={trip.date}
+      />
+    )
+  })
 
   return (
     <div>
       <Header />
       <button onClick={() => goToApplicationFormPage(navigate)}>Quero me inscrever</button>
       <h2>As melhores viagens!</h2>
+      <div>
+        {tripList}
+      </div>
     </div>
   )
 }
