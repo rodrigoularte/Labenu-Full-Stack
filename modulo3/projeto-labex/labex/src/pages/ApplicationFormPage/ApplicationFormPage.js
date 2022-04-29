c134382bca6cb34a0446b0851f0ad7fc5a5ba680
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2022-04-29 15:26:10 -0300

    fazendo o formulário no ApplicationFormPage

--- modulo3/projeto-labex/labex/src/pages/ApplicationFormPage/ApplicationFormPage.js
+++ modulo3/projeto-labex/labex/src/pages/ApplicationFormPage/ApplicationFormPage.js
@@ -4,11 +4,25 @@ import Header from "../../components/Header"
 import { urlBase } from "../../constants/constants"
 import { FormContainer } from "../../styled/FormContainer"
 import { ContentContainer, MainContainer } from "../../styled/PageStyled"
+import useForm from "../../hooks/useForm"
 
 const ApplicationFormPage = () => {
 
   const [trips, setTrips] = useState([])
+  // const [id, setId] = useState("")
 
+  const { form, onChange } = useForm(
+    {
+      name: "",
+      age: "",
+      applicationText: "",
+      profession: "",
+      country: "Brasil",
+      trip: ""
+    }
+  )
+
+  
   const getTrips = () => {
     axios
       .get(`${urlBase}/trips`)
@@ -20,11 +34,44 @@ const ApplicationFormPage = () => {
 
   useEffect(() => { getTrips() }, [])
 
+
+
+
+  const onSubmitApplication = (event) => {
+    //bloqueia o comportamento padrão do form
+    event.preventDefault()
+
+    console.log(form)
+  
+    // axios
+    // .post(`${urlBase}/login`, form)
+    // .then((res) => {
+    //   localStorage.setItem("token", res.data.token)
+    //   goToAdminHomePage(navigate)
+    // })
+    // .catch((err) => {alert(`ERRO! ${err.response.data.message}`)})
+  }
+
+
+
+  // const onChangeId = (event) => {
+  //   setId(event.target.value)
+  // }
+
+  // console.log(id)
+
+
+
   const tripList = trips.map((trip) => {
 
     return (
-      <option value={trip.id}>{`${trip.name} - ${trip.planet}`
-        }</option>
+      <option
+        id={trip.id}
+        name="trip"
+        value={trip.id}
+      >
+        {`${trip.name} - ${trip.planet}`}
+      </option>
     )
   })
 
@@ -33,17 +80,43 @@ const ApplicationFormPage = () => {
       <Header />
       <MainContainer>
         <ContentContainer>
-          <FormContainer  >
+          <FormContainer onSubmit={onSubmitApplication}>
             <h2>Inscreva-se para uma viagem</h2>
 
-            <select name="SelectTrip" id="" >
+            <select name="SelectTrip" id="SelectTrip" onChange={onChange}>
               <option value="Escolha uma viagem">Escolha uma viagem</option>
               {tripList}
             </select>
-            <input placeholder="Nome" />
-            <input placeholder="Idade" />
-            <input placeholder="Texto de candidatura" />
-            <input placeholder="Profissão" />
+            <input
+              name="name"
+              value={form.name}
+              type="text"
+              onChange={onChange}
+              placeholder="Nome"
+              required
+
+            />
+            <input
+              name="age"
+              value={form.age}
+              type="number"
+              onChange={onChange}
+              placeholder="Idade"
+            />
+            <input
+              name="applicationText"
+              value={form.applicationText}
+              type="text"
+              onChange={onChange}
+              placeholder="Texto de candidatura"
+            />
+            <input
+              name="profession"
+              value={form.profession}
+              type="text"
+              onChange={onChange}
+              placeholder="Profissão"
+            />
             <select name="Country" id="" >
               <option value="País">País</option>
             </select>
